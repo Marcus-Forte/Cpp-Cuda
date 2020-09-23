c9a179d5b527c299b64ede158430a7d53d680ee2
Marcus Forte <davi2812@dee.ufc.br>
2020-09-22 21:46:42 -0300

    added error check

--- src/main.cu
+++ src/main.cu
@@ -2,15 +2,19 @@
 #include <chrono>
 #include "vectorCPU.h"
 #include "vectorGPU.h"
-#include <cuda_profiler_api.h>
+
+
+int N = 10000000;
+
+
 // Host main routine0
 int main(int argc, char** argv){
 if(argc != 2){
-		std::cout << "Type number of elements .." << std::endl;
-		return -1;
+		std::cout << "Type number of elements ... using default N = " << N << std::endl;
+} else {
+N = atoi(argv[1]);
 }
 
-int N = atoi(argv[1]);
 
 float *a,*b,*c;
 a = (float*)malloc(sizeof(float)*N);
@@ -33,20 +37,57 @@ for(int i=0;i<N;++i){
 std::cout << "CPU OK -> " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " [ms]\n";
 // Precisamos alocar memória na GPU
 float *d_a,*d_b,*d_c;
-cudaMalloc((void**)&d_a,sizeof(float)*N);
-cudaMalloc((void**)&d_b,sizeof(float)*N);
-cudaMalloc((void**)&d_c,sizeof(float)*N);
-cudaMemcpy(d_a,a,sizeof(float)*N,cudaMemcpyHostToDevice);
-cudaMemcpy(d_b,b,sizeof(float)*N,cudaMemcpyHostToDevice);
+cudaError_t err;
+err = cudaMalloc((void**)&d_a,sizeof(float)*N);
+if(err != cudaSuccess){
+	std::cout << "GPU allocation error " << std::endl;
+		exit(EXIT_FAILURE);
+
+}
+err = cudaMalloc((void**)&d_b,sizeof(float)*N);
+if(err != cudaSuccess){
+	std::cout << "GPU allocation error " << std::endl;
+		exit(EXIT_FAILURE);
+
+}
+err = cudaMalloc((void**)&d_c,sizeof(float)*N);
+if(err != cudaSuccess){
+	std::cout << "GPU allocation error " << std::endl;
+		exit(EXIT_FAILURE);
+
+}
+err = cudaMemcpy(d_a,a,sizeof(float)*N,cudaMemcpyHostToDevice);
+if(err != cudaSuccess){
+	std::cout << "GPU memCpy error " << std::endl;
+		exit(EXIT_FAILURE);
+
+}
+err = cudaMemcpy(d_b,b,sizeof(float)*N,cudaMemcpyHostToDevice);
+if(err != cudaSuccess){
+	std::cout << "GPU memCpy error " << std::endl;
+		exit(EXIT_FAILURE);
+
+}
 
 
-int block_size = 256;
-int grid_size = ((N+block_size) / block_size);
+
+int block_size = 256; // 256
+int grid_size = (N + block_size - 1 )/block_size;
 std::cout << "grid size = " << grid_size << std::endl;
 //start = std::chrono::high_resolution_clock::now();
 vectorAddGPU<<<grid_size,block_size>>>(d_a,d_b,d_c,N);
 //end = std::chrono::high_resolution_clock::now();
+err = cudaGetLastError();
+
+if(err != cudaSuccess){
+		std::cout << "CUDA kernel launch error" << std::endl;
+		fprintf(stderr,"Error code %s",cudaGetErrorString(err));
+		exit(EXIT_FAILURE);
+}
+
+
 cudaDeviceSynchronize();
+
 float * c_fromgpu = (float*)malloc(sizeof(float)*N);
 cudaMemcpy(c_fromgpu,d_c,sizeof(float)*N,cudaMemcpyDeviceToHost);
 
@@ -67,7 +108,7 @@ float *s_a,*s_b,*s_c;
 cudaMallocManaged(&s_a,sizeof(float)*N);
 cudaMallocManaged(&s_b,sizeof(float)*N);
 cudaMallocManaged(&s_c,sizeof(float)*N);
-cudaProfilerStop();
+// cudaProfilerStop();
 // return 0; // Profiling won't work for shared memory
 for(int i=0;i<N;++i){
 s_a[i] = 1; s_b[i] = 2;
